cec0b7d31642e8522fd0780a5c8f74ab59c53892
Aedlatharungoud <aedlatharungoud@gmail.com>
2025-11-06 22:46:11 +0530

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,18 +1,18 @@
 <?php 
-// Database credentials
-define('DB_HOST', 'localhost');  // for Render DB, this will change later
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'tms');
+// DB credentials (from Clever Cloud)
+define('DB_HOST', 'behxgmihzluelavkvkjk-mysql.services.clever-cloud.com');
+define('DB_USER', 'ujalai0tgppgzjuh');
+define('DB_PASS', 'UG4Sw8OfGAeGRTPa3HEm');
+define('DB_NAME', 'behxgmihzluelavkvkjk');
 
+// Establish database connection
 try {
-    // Establish database connection
-    $options = array(
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-        PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
+    $dbh = new PDO(
+        "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";port=3306;charset=utf8",
+        DB_USER,
+        DB_PASS,
+        array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION)
     );
-
-    $dbh = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS, $options);
 } catch (PDOException $e) {
     exit("Error: " . $e->getMessage());
 }
