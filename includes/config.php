d1cf6af1b76001629d9c03c627693b69de93da17
Aedlatharungoud <aedlatharungoud@gmail.com>
2025-11-06 22:26:10 +0530

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,16 +1,19 @@
 <?php 
-// DB credentials.
-define('DB_HOST','localhost');
-define('DB_USER','root');
-define('DB_PASS','');
-define('DB_NAME','tms');
-// Establish database connection.
-try
-{
-$dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
-}
-catch (PDOException $e)
-{
-exit("Error: " . $e->getMessage());
+// Database credentials
+define('DB_HOST', 'localhost');  // for Render DB, this will change later
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'tms');
+
+try {
+    // Establish database connection
+    $options = array(
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
+    );
+
+    $dbh = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS, $options);
+} catch (PDOException $e) {
+    exit("Error: " . $e->getMessage());
 }
 ?>
